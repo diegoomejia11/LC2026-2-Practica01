985fb5d0e140b4cd73d925008a2aea06d0d3d8b5
Emily Zarate <emily.zarate.1802@gmail.com>
2026-02-19 20:30:06 -0600

    commit de Mily que espero se guarde

--- src/Practica01.hs
+++ src/Practica01.hs
@@ -12,12 +12,15 @@ data Shape = Circle Float | --representa el radio
 
 --Funcion que calcula el area de las figuras
 area :: Shape -> Float
-area = undefined
+area (Circle r) = pi * r * r
+area (Square s) = s * s
+area (Rectangle b h) = b * h
+area (Triangle s) = (sqrt 3 / 4) * s * s
+area (Trapeze b1 b2 h) = ((b1 + b2) / 2) * h    
 
 --Funcion que calcula el perimetro de las figuras
 perimeter :: Shape -> Float
 perimeter = undefined
-
 
 --Ejercicio 2 (Les toca arreglar el sinonimo)
 type Point = Shape
@@ -48,16 +51,30 @@ timeToWork = undefined
 --LISTAS Y FUNCIONES
 --Ejercicio 1
 palindromo :: String -> Bool
-palindromo = undefined
+palindromo [] = True
+palindromo [_] = True
+-- comparamos la cabeza de la cadena con la cola, usamos && para que
+-- de ser igual la cabeza y la cola se llama a la función nuevamente 
+-- en la sig llamada comienza desde el siguiente elemento del string del inicio y el final
+palindromo xs = (head xs == last xs) && palindromo (init(tail xs))
+
 
 --Ejercicio 2
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = undefined
+myFoldr f z [] = z
+-- Ocupamos una función, un valor inicial(base) y una lista sobre la cual trabajaremos
+-- Se ejecuta la función en el primer elemento y se realiza la llamada recursiva para el resto
+myFoldr f z (x:xs) = f x (myFoldr f z (xs))
 
 --Ejercicio 3
 conjuntoPotencia :: [a] -> [[a]]
-conjuntoPotencia = undefined
+-- El conjuto potencia de la lista vacia es una lista con la lista vacia como elemento
+conjuntoPotencia [] =[[]] 
+-- Genera todas las listas que contienen a x y lo concatenamos con las listas que no contienen a x como elemento
+conjuntoPotencia (x:xs) = [x:ys| ys <- conjuntoPotencia xs] ++ conjuntoPotencia xs 
 
+
+--conjuntoPotencia = 
 --ARBOLES
 
 --Implementacion
