79943ee26b66e47da71a27c7338f1e8346184c6f
Torres-kaze <torres.kaze20@ciencias.unam.mx>
2026-02-19 20:39:36 -0600

    Add files via upload

---
+++ Practica01.hs
@@ -0,0 +1,79 @@
+module Practica01 where
+
+--TIPOS ALGEBRAICOS
+
+--Ejercicio 1
+--Ejercicio 1
+data Shape = Circle Float | --representa el radio
+            Square Float | --representa un lado
+            Rectangle Float Float| --representa base y altura
+            Triangle Float | --representa un lado
+            Trapeze Float Float Float --representa base mayor, base menor y altura
+            deriving (Show)
+
+--Funcion que calcula el area de las figuras
+area :: Shape -> Float
+area (Circle r) = pi * r * r
+area (Square s) = s * s
+area (Rectangle b h) = b * h
+area (Triangle s) = (sqrt 3 / 4) * s * s
+area (Trapeze b1 b2 h) = ((b1 + b2) / 2) * h    
+
+--Funcion que calcula el perimetro de las figuras
+perimeter :: Shape -> Float
+perimeter (Trapeze b1 b2 h) = 2 * (sqrt (((b1 - b2)/2)^2 + h^2)) + b1 + b2
+
+
+--Ejercicio 2 (Les toca arreglar el sinonimo)
+type Point = Shape
+
+-- Funcion para calcular la distancia entre dos puntos
+distance :: Point -> Point -> Float
+distance = undefined 
+
+--Funcion para calcular la distancia de un punto al origen
+from0 :: Point -> Float
+from0 = undefined
+
+--Ejercicio 3
+data Haskellium = Undefined
+
+--Funcion para regresar el hijo de dos Haskelliums dado su nombre
+son :: Haskellium -> Haskellium -> String -> Haskellium
+son = undefined
+
+--Funcion para calcular las unidades para construir la casa de un Haskellium
+houseCost :: Haskellium -> Float
+houseCost = undefined
+
+--Funcion para calcular el tiempo que le toma a un Haskellium para llegar a su trabajo
+timeToWork :: Haskellium -> Float
+timeToWork = undefined
+
+--LISTAS Y FUNCIONES
+--Ejercicio 1
+palindromo :: String -> Bool
+palindromo = undefined
+
+--Ejercicio 2
+myFoldr :: (a -> b -> b) -> b -> [a] -> b
+myFoldr = undefined
+
+--Ejercicio 3
+conjuntoPotencia :: [a] -> [[a]]
+conjuntoPotencia = undefined
+
+--ARBOLES
+
+
+--Implementacion
+
+data OneTwoTree a = Vacio 
+                    | Nudo (OneTwoTree a) a (OneTwoTree a)
+                    deriving (Show, Eq, Ord)
+
+
+--Ejercicio 2
+suma :: OneTwoTree Int -> Int
+suma Vacio = 0
+suma (Nudo a valor b) = valor + (suma a) + (suma b)
